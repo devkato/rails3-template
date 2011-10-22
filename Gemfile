874d4d3483c29ede769746c0401c31171acbf7de
Hiroyuki Kato <dev.kato@gmail.com>
2011-10-23 01:29:36 +0900

    add devise initialization

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'http://rubygems.org'
 
-gem 'rails',     '=3.1.1'
-gem 'mysql2',    '=0.3.7'
-gem 'kaminari',  '=0.12.4'
+gem 'rails', '=3.1.1'
+gem 'mysql2', '=0.3.7'
+gem 'kaminari', '=0.12.4'
+gem 'devise', '=1.4.9'
 
 
 # Gems used only for assets and not required
--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,8 @@ module Rails3Template
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -50,5 +51,8 @@ module Rails3Template
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for deploying with Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
